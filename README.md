d7f9b5058a93f371df708f388ac08154bebecf3d
Adam Volný <adam.volny@gmail.com>
2021-11-12 14:10:15 +0100

    add README.md and LICENSE.txt, adjust setup

---
+++ README.md
@@ -0,0 +1,31 @@
+# Board Game Factory
+
+README IS IN PROGRESS
+
+This is a vector-graphics framework intended for creating
+and scaling up production of reusable assets with vector graphics.
+
+I've developed it personally to satisfy my need for a solid prototyping
+engine for board games, but inherently, it is in no way tied to making board games,
+even though it contains a couple convenience classes for that (such as CardSheet for 
+producing printable card sheets out of lists of components).
+
+## Features
+
+ - Component Tree structure - similar to making GUIs in Java.
+ - Many basic components implemented:
+   - Shapes:
+     - Rectangle
+
+##
+
+
+## Usage
+
+For examples of usage see tests.
+
+## Things I made with BGF
+
+Samples of prototypes I've made with this framework:
+
+https://imgur.com/a/TS779gR
--- setup.py
+++ setup.py
@@ -3,11 +3,30 @@
 from distutils.core import setup
 
 setup(name='bgfactory',
-      version='1.0',
-      description='Board Game Factory',
+      version='0.1',
+      description='Board Game Factory - a framework for composing reusable vector graphics assets, e.g. for board games',
       author='Adam Volný',
       author_email='adam.volny@gmail.com',
       # url='https://www.python.org/sigs/distutils-sig/',
       packages=['bgfactory', 'bgfactory.common', 'bgfactory.components'],
-      package_dir = {'': 'src'}
+      package_dir = {'': 'src'},
+      # url='https://github.com/user/reponame',  # Provide either the link to your github or to your website
+      # download_url='https://github.com/user/reponame/archive/v_01.tar.gz',  # I explain this later on
+      keywords=['board', 'game', 'factory', 'bgf', 'vector', 'graphics'],  # Keywords that define your package best
+      install_requires=[  # I get to this in a second
+            'cairocffi'
+            'pangocffi',
+            'pillow'
+      ],
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+            'Intended Audience :: Developers',  # Define that your audience are developers
+            'Topic :: Graphics :: Vector Graphics',
+            'License :: OSI Approved :: MIT License',  # Again, pick a license
+            # 'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+            # 'Programming Language :: Python :: 3.4',
+            # 'Programming Language :: Python :: 3.5',
+            # 'Programming Language :: Python :: 3.6',
+      ],
      )
