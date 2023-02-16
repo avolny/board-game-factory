cee64699ffda65c062172811cdd61ee229784448
adam.volny <adam.volny@gmail.com>
2023-02-16 10:55:38 +0100

    bgf.py -> ff.py | This is a major change, bgf was annoying to type, ff might be the best acronym available
     - this is intended as the main usage pattern of the framework:
         - use from bgfactory import ff
         - and then access everything with ff.*, like ff.Rectangle, ff.make_printable_sheets, etc.
     - as functionality is added, everything that is intended to be public will be exposed here.

---
+++ src/bgfactory/ff.py
@@ -0,0 +1,15 @@
+
+from bgfactory.components.component import Component, Container
+from bgfactory.components.constants import *
+from bgfactory.components.layout.absolute_layout import AbsoluteLayout
+from bgfactory.components.layout.layout_manager import LayoutManager, LayoutError
+from bgfactory.components.layout.vertical_flow_layout import VerticalFlowLayout
+from bgfactory.components.layout.horizontal_flow_layout import HorizontalFlowLayout
+from bgfactory.components.cairo_helpers import image_from_surface
+from bgfactory.components.card_sheet import CardSheet, make_printable_sheets
+from bgfactory.components.grid import Grid, GridCell, GridError
+from bgfactory.components.shape import Shape, Rectangle, Circle, RoundedRectangle, Line
+from bgfactory.components.regular_polygon import RegularPolygon
+from bgfactory.components.source import PNGSource, RGBSource, RGBASource, Source, AUTO
+from bgfactory.components.text import TextMarkup, TextUniform, FontDescription
+from bgfactory.components.utils import A4_WIDTH_MM, MM_PER_INCH, A4_HEIGHT_MM, mm_to_pixels, get_a4_pixel_size
