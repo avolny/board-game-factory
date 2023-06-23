defee6071963ecebe8f03331887e02eb99677cb5
adam.volny <adam.volny@gmail.com>
2023-06-23 16:15:27 +0200

    add hex_color_to_rgba helper function

--- bgfactory/components/utils.py
+++ bgfactory/components/utils.py
@@ -1,3 +1,4 @@
+import re
 
 
 def parse_percent(val):
@@ -28,3 +29,13 @@ def mm_to_pixels(mm, dpi=300):
 
 def get_a4_pixel_size(dpi=300):
     return mm_to_pixels(A4_WIDTH_MM, dpi), mm_to_pixels(A4_HEIGHT_MM, dpi)
+
+
+def hex_color_to_rgba(hex):
+    match = re.match(r'^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$', hex, re.IGNORECASE)
+    if not match:
+        raise ValueError('Invalid hex color: "{}"'.format(hex))
+
+    r, g, b = match.group(1), match.group(2), match.group(3)
+
+    return int(r, 16) / 255, int(g, 16) / 255, int(b, 16) / 255, 255 / 255
